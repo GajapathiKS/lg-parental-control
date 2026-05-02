80584095786613b2158e395b3ca7ce2487c20237
Gajapathi K S <64843288+GajapathiKS@users.noreply.github.com>
2026-05-02 10:59:22 +0530

    Disable viewport scaling on webOS TV

--- js/viewport.js
+++ js/viewport.js
@@ -6,14 +6,20 @@ var Viewport = (function () {
   var BASE_HEIGHT = 1080;
 
   function init() {
+    if (_isWebOsTv()) return;
+    document.documentElement.className += ' viewport-scaled';
     fit();
     window.addEventListener('resize', fit);
     window.addEventListener('orientationchange', fit);
   }
 
   function fit() {
-    var width = window.innerWidth || BASE_WIDTH;
-    var height = window.innerHeight || BASE_HEIGHT;
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || BASE_WIDTH;
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || BASE_HEIGHT;
+    if (width < 320 || height < 240) {
+      width = BASE_WIDTH;
+      height = BASE_HEIGHT;
+    }
     var scale = Math.min(width / BASE_WIDTH, height / BASE_HEIGHT);
     var left = Math.max(0, (width - BASE_WIDTH * scale) / 2);
     var top = Math.max(0, (height - BASE_HEIGHT * scale) / 2);
@@ -28,4 +34,9 @@ var Viewport = (function () {
     init: init,
     fit: fit,
   };
+
+  function _isWebOsTv() {
+    if (window.PalmSystem) return true;
+    return !!(window.webOS && window.webOS.platform && window.webOS.platform.tv);
+  }
 })();
